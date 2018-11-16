9bd018ff8de7b7a0430bdd21305de46780c4d9de
Edu <edu.d.rai@gmail.com>
2018-11-16 19:55:15 -0300

    Updates

---
+++ precache-manifest.5e253eae4e6f32f330d75b605dfbbe55.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "cceb22e48041aa0ecdec",
+    "url": "/portfolio/static/js/main.cceb22e4.chunk.js"
+  },
+  {
+    "revision": "1da05519fa47cd3184a3",
+    "url": "/portfolio/static/js/1.1da05519.chunk.js"
+  },
+  {
+    "revision": "cceb22e48041aa0ecdec",
+    "url": "/portfolio/static/css/main.a518bbb7.chunk.css"
+  },
+  {
+    "revision": "6ffe186d422d6695e30263ffdf704e08",
+    "url": "/portfolio/index.html"
+  }
+];
